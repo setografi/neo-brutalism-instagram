915a24d6808c03e8bc96c100e2cad3f0a52406e8
setografi <hitonaashbell@gmail.com>
2024-04-17 22:48:54 +0700

    update: Navbar and Stories section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        backgroundColor: "#ECE6C2",
+        textBlack: "#000000",
+        goldenRod: "#D0B229",
+        roseQuartz: "#C58F9B",
+        limeGreen: "#73C255",
+        ceruleanBlue: "#3C86BA",
+        turQuoise: "#55C2B9",
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
