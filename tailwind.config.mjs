5ddfbceda29fd5e8d8cf8a552a44fd7470a56eb6
setografi <hitonaashbell@gmail.com>
2024-04-24 16:44:52 +0700

    update: ProfilePage 60%

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,15 +8,11 @@ export default {
         textBlack: "#000000",
         grayBgpost: "#DFDFE8",
         dimGray: "#6E6E6E",
-
-        goldenRod: "#D0B229",
-        roseQuartz: "#C58F9B",
-        limeGreen: "#73C255",
-        ceruleanBlue: "#3C86BA",
-        turQuoise: "#55C2B9",
+        textGray: "#727272",
       },
       fontFamily: {
         inter: ["Inter", "sans-serif"],
+        sfPro: ["SF Pro Text", "sans-serif"],
       },
     },
   },
